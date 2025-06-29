7cc0603d42ce907d7e5a8b5b0559594aaed65ede
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2025-06-29 12:41:43 +0200

    Add a failing test

--- test_main.py
+++ test_main.py
@@ -9,7 +9,8 @@ from main import parse_note_body
      'Archive https://rustle.ca/posts/articles/work-from-home-lighting'),
     ('<html><head></head><body style="overflow-wrap: break-word; -webkit-nbsp-mode: space; line-break: after-white-space;">Quote:&nbsp;<span style="-webkit-text-size-adjust: auto; caret-color: rgb(51, 51, 51); color: rgb(51, 51, 51); font-family: sans-serif; font-size: 16.200001px;">r. When working with someone on a project don\'t "design an interface" and then work alone on either side of it. Work together all over it.</span></body></html>',
      'Quote: r. When working with someone on a project don\'t "design an interface" and then work alone on either side of it. Work together all over it.',
-     )
+     ),
+    ('<html><head></head><body style="overflow-wrap: break-word; -webkit-nbsp-mode: space; line-break:     after-white-space;"><ol><li>1. READACTED 2. READACTED</li></ol></body></html>', "1. READACTED 2. READACTED"),
 ])
 def test_parse_note_body(note_body, expected_result):
     assert parse_note_body(note_body) == expected_result
